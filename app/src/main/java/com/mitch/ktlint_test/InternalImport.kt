45d0284e94eba6a2e07f026ce5c679a7e8bd8f37
Mitch Thornton <mthornton@gmail.com>
2020-10-22 17:22:03 -0700

    Got Detekt Working

---
+++ app/src/main/java/com/mitch/ktlint_test/InternalImport.kt
@@ -0,0 +1,13 @@
+/**
+ * Copyright Tandem Diabetes Care, Inc. 2020.  All rights reserved.
+ * InternalImport.kt
+ * @summary:
+ * @author: Mitch Thornton Oct 22, 2020
+ */
+
+package com.mitch.ktlint_test
+
+class InternalImport(val internalImport: InternalClass) {
+
+    class InternalClass()
+}
--- app/src/main/java/com/mitch/ktlint_test/MainActivity.kt
+++ app/src/main/java/com/mitch/ktlint_test/MainActivity.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBar
+import java.lang.Math.PI
 
-class MainActivity(val testOne: Int, val testTwo: Int, val testThree: Int, val testFive: Int, val testFour: Int) : AppCompatActivity() {
+class MainActivity(
+    val testOne: Int,
+    val testTwo: Int,
+    val testThree: Int,
+    val testFive: Int,
+    val testFour: Int
+) : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
